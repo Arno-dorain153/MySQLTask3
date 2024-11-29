1747a0bd76107e371f0acbceb477f30ad24a722a
Arno-dorain153 <arnodorain153@gmail.com>
2024-11-29 07:41:30 +0530

    Add files via upload

---
+++ task2.sql
@@ -0,0 +1,4 @@
+SELECT * FROM student.new_marklist1;
+alter table student.new_marklist1 modify id int first ;
+alter table student.new_marklist1 add ph int;
+alter table student.new_marklist1 modify ph varchar(15);
